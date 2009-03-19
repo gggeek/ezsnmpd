7766d88be752f737531101044f398fb753952253
gggeek <gg@ez.no>
2009-03-19 08:27:23 +0000

    - switch default root oid to eZ Systems official PEN number

--- settings/snmpd.ini.append.php
+++ settings/snmpd.ini.append.php
@@ -4,9 +4,9 @@
 
 # The base prefix for all the OIDs we answer for.
 # Always keeping within within iso.org.dod.internet.private.enterprise (1.3.6.1.4.1) is a good idea
-# .eZ Systmes (.35000)
+# .eZ Systmes (.33120) - see http://www.iana.org/assignments/enterprise-numbers
 # .eZ Publish (.1)
-Prefix=1.3.6.1.4.1.35000.1
+Prefix=1.3.6.1.4.1.33120.1
 
 # List of php classes delegated to answer GET/SET responses
 # each one of those takes care of a list of OIDs, that form the overall eZ Publish MIB
