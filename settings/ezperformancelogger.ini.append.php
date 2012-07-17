8dc750cd63813d1d6458680aaac0ec36ae1b64ce
gggeek <gg@ez.no>
2012-07-18 00:58:14 +0200

    Add forgotten file for ezperlogger integration

---
+++ settings/ezperformancelogger.ini.append.php
@@ -0,0 +1,9 @@
+<?php /*
+
+[GeneralSettings]
+VariableProviders[]=eZsnmpdPerfLogger
+
+# Example settings to log via eZPErformanceLogger value available as SNMP oids:
+# (look up list of available oids in admin interface | Setup | SNMP Monitoring)
+#TrackVariables[]=snmp/contentObjects
+#TrackVariables[]=snmp/1.3.6.1.4.1.33120.1.2.1.3.1
